7c0d7551d4a1438643ba6cee1ec0b9d1c3cd91c1
anton.kudruk <anton.kudruk@digital.originenterprises.com>
2026-09-18 02:03:33 +0300

    Fixed periodic table

--- examples/periodic-table/src/domain/periodicTableModel.ts
+++ examples/periodic-table/src/domain/periodicTableModel.ts
@@ -1,5 +1,14 @@
+import { SelectController } from "xorlab-interactive";
 import { createPeriodicTableLayout } from "./periodicTable";
 import { ChemicalElement } from "./types";
+import { ICard, ISeg } from "xorlab";
+
+export interface PeriodicTableModel {
+  readonly elementSelect: SelectController<ChemicalElement>;
+  readonly rowsHeaderSeg: ISeg;
+  readonly columnsHeaderSeg: ISeg;
+  readonly widgetCard: ICard;
+}
 
 export function createPeriodicTableModel(
   elements: ChemicalElement[]
